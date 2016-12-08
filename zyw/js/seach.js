7571d44f9f7969c2ca2324d1a298072c7b953245
huangyongkang <634475859@qq.com>
2016-12-08 21:51:43 +0800

    hyk-list

---
+++ zyw/js/seach.js
@@ -0,0 +1,15 @@
+$(function(){
+	var i =0;
+	$(".list-big").on("touchstart",function(){
+		i++
+		if(i%2 ==0){
+			$("p.yk-list").css("display","none")
+			$(this).children("i").html("&#xe501;")	
+		}else{
+			$("p.yk-list").css("display","block")
+			$(this).children("i").html("&#xe656;")	
+		}
+		
+	})
+	
+})
