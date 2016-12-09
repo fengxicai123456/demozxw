fd2e05d4f315a35d2b42749c371666389911f481
huangyongkang <634475859@qq.com>
2016-12-09 11:34:19 +0800

    hyk-change-seach-js

--- zyw/js/seach.js
+++ zyw/js/seach.js
@@ -3,10 +3,10 @@ $(function(){
 	$(".list-big").on("touchstart",function(){
 		i++
 		if(i%2 ==0){
-			$("p.yk-list").css("display","none")
+			$("p.yk-list").css("display","block")
 			$(this).children("i").html("&#xe501;")	
 		}else{
-			$("p.yk-list").css("display","block")
+			$("p.yk-list").css("display","none")
 			$(this).children("i").html("&#xe656;")	
 		}
 		
