e060098e7cf16358f5505f78af4aa359f3349f79
liming <2528747512@qq.com>
2016-12-10 10:30:45 +0800

    wode

--- zyw/js/more.js
+++ zyw/js/more.js
@@ -2,9 +2,47 @@ var flag=true;
 $('.serviceone .test').on('touchstart',function(){
 	if(flag){
 	$(this).children('.m-jiantou').css('transform','rotateZ(90deg)')
+	$(".test").children('em').css("display","none");
+	$(this).children('.dew').css("display","block");
+	if($(this).html()==$('.serviceone .test1').html()){		
+		$(".de").css("display","block");		
+	}
 	flag=!flag;
 	}else{
 	$(this).children('.m-jiantou').css('transform','rotateZ(0deg)')	
+		$(this).children('.dew').css("display","none");
 	flag=!flag;
 	} 
-})
+});
+$(".de .deri").on('touchstart',function(){
+$(".de").css("display","none");
+});
+
+$('.servicetwo .test').on('touchstart',function(){	
+if(flag){ 
+	$(this).children('.m-jiantou').css('transform','rotateZ(90deg)')
+	$(this).children(".ta").css("display","block");
+flag=!flag;	
+}else{	
+flag=!flag;	
+$(this).children('.m-jiantou').css('transform','rotateZ(0deg)')
+}
+});
+$('.servicetwo .test .ta3').on('touchstart',function(){	
+$(".ta").css("display","none");
+});
+
+
+	window.onload = function(){
+		var zxyUser  = localStorage.getItem("userName");
+		if(zxyUser){
+			$(".out").html("退出登录");
+		$('.out').on('touchend',function(){
+   			localStorage.removeItem("userName")
+   			$(".out").html("请登入");
+   		})
+		}else{
+		$(".out").html("请登入");
+		}
+   	
+   		}
